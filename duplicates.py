d327a490e76113ff90ab6d22ff1eb5abee3550e2
sowjanya <sowjanyamekapothula@gmail.com>
2026-09-24 12:34:27 +0530

    upload code

---
+++ duplicates.py
@@ -0,0 +1,9 @@
+def has_duplicates(arr):
+    seen=set()
+    for num in arr:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+arr=[1,2,3]
+print(has_duplicates(arr))
